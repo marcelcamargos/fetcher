2b630c35fd2797b599adedafcaa76ee54b4d2977
Marcel Gonçalves Viana Marins de Camargos <marcel.camargos@gmail.com>
2023-02-06 21:20:01 -0300

    project initial creation

---
+++ Fetcher/Fetcher/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Fetcher
+//
+//  Created by Marcel Camargos on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
